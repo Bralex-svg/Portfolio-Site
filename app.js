bb5619be0b36fa3f3352ee10f65c410d0a9d93c3
Bralex-svg <alexadam269@gmail.com>
2021-05-15 15:34:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile-menu')
+const menuList = document.querySelector('.nav-menu')
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('is-active')
+    menuList.classList.toggle('active')
+})
